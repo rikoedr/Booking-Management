b109aab86b19ed25b6c78d063d140df3fcb1c51f
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-10-04 16:46:32 +0800

    API Project: Fixed missing attributes in CreateEmployeeDTO

--- API/DataTransferObjects/Creates/CreateEmployeeDTO.cs
+++ API/DataTransferObjects/Creates/CreateEmployeeDTO.cs
@@ -7,6 +7,7 @@ public class CreateEmployeeDTO
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime BirthDate { get; set; }
+    public int Gender { get; set; }
     public DateTime HiringDate { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
@@ -21,6 +22,7 @@ public class CreateEmployeeDTO
             FirstName = employeeDTO.FirstName,
             LastName = employeeDTO.LastName,
             BirthDate = employeeDTO.BirthDate,
+            Gender = employeeDTO.Gender,
             HiringDate = employeeDTO.HiringDate,
             Email = employeeDTO.Email,
             PhoneNumber = employeeDTO.PhoneNumber
