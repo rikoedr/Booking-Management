a12a250a0bc584e4e2d466471d42012ad5efc90f
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-10-04 10:51:59 +0800

    API Project: Update Booking Route API Response Format and Add Error Handling

--- API/Controllers/BookingController.cs
+++ API/Controllers/BookingController.cs
@@ -3,16 +3,27 @@ using API.Models;
 using Microsoft.AspNetCore.Mvc;
 using API.DataTransferObjects;
 using API.DataTransferObjects.Creates;
+using API.Utilities.Handlers;
+using API.Utilities;
+using System.Net;
+using API.Repositories;
 
 namespace API.Controllers;
+
+/*
+ * Booking Controller adalah class untuk yang mengatur penerimaan request dan pengembalian response API.
+ * Class ini terhubung dengan class Booking Repository yang berfungsi untuk melakukan ORM.
+ * Success dan Error Response di dalam controller ini di handle oleh ControllerBase dan Utility Class
+ * terkait format Response API.
+ */
 
 [ApiController]
 [Route("api/[controller]")]
 public class BookingController : ControllerBase
 {
-    private readonly IGeneralRepository<Booking> _repository;
+    private readonly BookingRepository _repository;
 
-    public BookingController(IGeneralRepository<Booking> repository)
+    public BookingController(BookingRepository repository)
     {
         _repository = repository;
     }
@@ -20,84 +31,172 @@ public class BookingController : ControllerBase
     [HttpGet]
     public IActionResult GetAll()
     {
-        var result = _repository.GetAll();
+        // Get data collection from repository
+        IEnumerable<Booking> dataCollection = _repository.GetAll();
 
-        if (!result.Any())
+        // Handling null data
+        if (!dataCollection.Any())
         {
-            return NotFound("Data not Found");
+            return NotFound(new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = Message.DataNotFound
+
+            });
         }
 
-        var data = result.Select(item => (BookingDTO)item);
+        // Return success response
+        IEnumerable<BookingDTO> data = dataCollection.Select(item => (BookingDTO)item);
+        ResponseOKHandler<IEnumerable<BookingDTO>> response = new ResponseOKHandler<IEnumerable<BookingDTO>>(data);
 
-        return Ok(data);
+        return Ok(response);
     }
 
     [HttpGet("{guid}")]
     public IActionResult GetByGuid(Guid guid)
     {
-        var result = _repository.GetByGuid(guid);
+        // Check if data available
+        Booking? data = _repository.GetByGuid(guid);
 
-        if (result is null)
+        // Handling request if data is not found
+        if (data is null)
         {
-            return NotFound("Id not found");
+            return NotFound(new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = Message.DataNotFound
+            });
         }
 
-        return Ok((BookingDTO)result);
+        // Return success response 
+        BookingDTO response = (BookingDTO)data;
+
+        return Ok(response);
     }
 
     [HttpPost]
     public IActionResult Create(CreateBookingDTO bookingDTO)
     {
-        var result = _repository.Create(bookingDTO);
-
-        if (result is null)
+        try
         {
-            return BadRequest("Failed to create data");
-        }
+            // Create data from request paylod
+            Booking? result = _repository.Create(bookingDTO);
 
-        return Ok((BookingDTO)result);
+            // Return success response
+            ResponseOKHandler<BookingDTO> response = new ResponseOKHandler<BookingDTO>((BookingDTO)result);
+
+            return Ok(response);
+        }
+        catch (ExceptionHandler ex)
+        {
+            ResponseErrorHandler response = new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(),
+                Message = Message.FailedToCreateData,
+                Error = ex.Message
+            };
+
+            return StatusCode(StatusCodes.Status500InternalServerError, response);
+        }
     }
 
     [HttpPut]
     public IActionResult Update(BookingDTO bookingDTO)
     {
-        var entity = _repository.GetByGuid(bookingDTO.Guid);
-
-        if (entity is null)
+        try
         {
-            return NotFound("Id not found");
+            // Check if data available
+            Booking? entity = _repository.GetByGuid(bookingDTO.Guid);
+
+            // Handling null data
+            if (entity is null)
+            {
+                return NotFound(new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = Message.DataNotFound
+                });
+            }
+
+            // Update data
+            Booking toUpdate = bookingDTO;
+            toUpdate.CreatedDate = entity.CreatedDate;
+
+            bool result = _repository.Update(toUpdate);
+
+            // Throw an exception if update failed
+            if (!result)
+            {
+                throw new ExceptionHandler(Message.ErrorOnUpdatingData);
+            }
+
+            // Return success response
+            ResponseOKHandler<string> response = new ResponseOKHandler<string>(Message.DataUpdated);
+
+            return Ok(response);
         }
-
-        Booking toUpdate = bookingDTO;
-        toUpdate.CreatedDate = entity.CreatedDate;
-
-        var result = _repository.Update(toUpdate);
-
-        if (!result)
+        catch (ExceptionHandler ex)
         {
-            return BadRequest("Failed to update data");
-        }
+            ResponseErrorHandler response = new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(),
+                Message = Message.FailedToCreateData,
+                Error = ex.Message
+            };
 
-        return Ok("Data updated");
+            return StatusCode(StatusCodes.Status500InternalServerError, response);
+        }
     }
 
     [HttpDelete("{guid}")]
     public IActionResult Delete(Guid guid)
     {
-        var entity = _repository.GetByGuid(guid);
-
-        if (entity is null)
+        try
         {
-            return NotFound("Id not found");
+            // Check if data available
+            Booking? data = _repository.GetByGuid(guid);
+
+            // Handling null data
+            if (data is null)
+            {
+                return NotFound(new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = Message.DataNotFound
+                });
+            }
+
+            // Delete data
+            bool result = _repository.Delete(data);
+
+            // Throw an exception if update failed
+            if (!result)
+            {
+                throw new ExceptionHandler(Message.ErrorOnDeletingData);
+            }
+
+            // Return success response
+            ResponseOKHandler<string> response = new ResponseOKHandler<string>(Message.DataDeleted);
+
+            return Ok(response);
         }
-
-        var result = _repository.Delete(entity);
-
-        if (!result)
+        catch (ExceptionHandler ex)
         {
-            return BadRequest("Failed to delete data");
-        }
+            ResponseErrorHandler response = new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(),
+                Message = Message.FailedToCreateData,
+                Error = ex.Message
+            };
 
-        return Ok("Data deleted");
+            return StatusCode(StatusCodes.Status500InternalServerError, response);
+        }
     }
 }
