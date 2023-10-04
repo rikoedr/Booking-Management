189a1590ab64d54c740d35044a5cb84d210b8b31
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-10-04 10:50:53 +0800

    API Project: Update Account Route API Response Format and Add Error Handling

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,16 +4,26 @@ using Microsoft.AspNetCore.Mvc;
 using API.DataTransferObjects;
 using API.DataTransferObjects.Creates;
 using API.Utilities.Handlers;
+using API.Utilities;
+using System.Net;
+using API.Repositories;
 
 namespace API.Controllers;
+
+/*
+ * Account Controller adalah class untuk yang mengatur penerimaan request dan pengembalian response API.
+ * Class ini terhubung dengan class Account Repository yang berfungsi untuk melakukan ORM.
+ * Success dan Error Response di dalam controller ini di handle oleh ControllerBase dan Utility Class
+ * terkait format Response API.
+ */
 
 [ApiController]
 [Route("api/[controller]")]
 public class AccountController : ControllerBase
 {
-    private readonly IGeneralRepository<Account> _repository;
+    private readonly AccountRepository _repository;
 
-    public AccountController(IGeneralRepository<Account> repository)
+    public AccountController(AccountRepository repository)
     {
         _repository = repository;
     }
@@ -21,88 +31,174 @@ public class AccountController : ControllerBase
     [HttpGet]
     public IActionResult GetAll()
     {
-        var result = _repository.GetAll();
+        // Get data collection from repository
+        IEnumerable<Account> dataCollection = _repository.GetAll();
 
-        if (!result.Any())
+        // Handling null data
+        if (!dataCollection.Any())
         {
-            return NotFound("Data not Found");
+            return NotFound(new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = Message.DataNotFound
+
+            });
         }
 
-        var data = result.Select(item => (AccountDTO)item);
+        // Return success response
+        IEnumerable<AccountDTO> data = dataCollection.Select(item => (AccountDTO)item);
+        ResponseOKHandler<IEnumerable<AccountDTO>> response = new ResponseOKHandler<IEnumerable<AccountDTO>>(data);
 
-        return Ok(data);
+        return Ok(response);
     }
 
     [HttpGet("{guid}")]
     public IActionResult GetByGuid(Guid guid)
     {
-        var result = _repository.GetByGuid(guid);
+        // Check if data available
+        Account? data = _repository.GetByGuid(guid);
 
-        if (result is null)
+        // Handling request if data is not found
+        if (data is null)
         {
-            return NotFound("Id not found");
+            return NotFound(new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = Message.DataNotFound
+            });
         }
 
-        return Ok((AccountDTO)result);
+        // Return success response 
+        AccountDTO response = (AccountDTO)data;
+
+        return Ok(response);
     }
 
     [HttpPost]
     public IActionResult Create(CreateAccountDTO accountDTO)
     {
-        var toCreate = accountDTO;
-        toCreate.Password = HashingHandler.HashPassword(toCreate.Password);
-
-        var result = _repository.Create(toCreate);
-
-        if (result is null)
+        try
         {
-            return BadRequest("Failed to create data");
-        }
+            // Create data from request paylod
+            Account toCreate = accountDTO;
+            toCreate.Password = HashingHandler.HashPassword(accountDTO.Password);
 
-        return Ok((AccountDTO)result);
+            // Return success response
+            Account? result = _repository.Create(toCreate);
+            ResponseOKHandler<AccountDTO> response = new ResponseOKHandler<AccountDTO>((AccountDTO)result);
+
+            return Ok(response);
+        }
+        catch (ExceptionHandler ex)
+        {
+            ResponseErrorHandler response = new ResponseErrorHandler()
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(),
+                Message = Message.FailedToCreateData,
+                Error = ex.Message
+            };
+
+            return StatusCode(StatusCodes.Status500InternalServerError, response);
+        }
     }
 
     [HttpPut]
     public IActionResult Update(AccountDTO accountDTO)
     {
-        var entity = _repository.GetByGuid(accountDTO.Guid);
-
-        if (entity is null)
+        try
         {
-            return NotFound("Id not found");
+            // Check if data available
+            Account? entity = _repository.GetByGuid(accountDTO.Guid);
+
+            // Handling null data
+            if (entity is null)
+            {
+                return NotFound(new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = Message.DataNotFound
+                });
+            }
+
+            // Update data
+            Account toUpdate = accountDTO;
+            toUpdate.CreatedDate = entity.CreatedDate;
+
+            bool result = _repository.Update(toUpdate);
+
+            // Throw an exception if update failed
+            if (!result)
+            {
+                throw new ExceptionHandler(Message.ErrorOnUpdatingData);
+            }
+
+            // Return success response
+            ResponseOKHandler<string> response = new ResponseOKHandler<string>(Message.DataUpdated);
+
+            return Ok(response);
         }
-
-        Account toUpdate = accountDTO;
-        toUpdate.CreatedDate = entity.CreatedDate;
-        toUpdate.Password = HashingHandler.HashPassword(toUpdate.Password);
-
-        var result = _repository.Update(toUpdate);
-
-        if (!result)
+        catch (ExceptionHandler ex)
         {
-            return BadRequest("Failed to update data");
-        }
+            ResponseErrorHandler response = new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(),
+                Message = Message.FailedToCreateData,
+                Error = ex.Message
+            };
 
-        return Ok("Data updated");
+            return StatusCode(StatusCodes.Status500InternalServerError, response);
+        }
     }
 
     [HttpDelete("{guid}")]
     public IActionResult Delete(Guid guid)
     {
-        var entity = _repository.GetByGuid(guid);
-
-        if (entity is null)
+        try
         {
-            return NotFound("Id not found");
+            // Check if data available
+            Account? data = _repository.GetByGuid(guid);
+
+            // Handling null data
+            if (data is null)
+            {
+                return NotFound(new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = Message.DataNotFound
+                });
+            }
+
+            // Delete data
+            bool result = _repository.Delete(data);
+
+            // Throw an exception if update failed
+            if (!result)
+            {
+                throw new ExceptionHandler(Message.ErrorOnDeletingData);
+            }
+
+            // Return success response
+            ResponseOKHandler<string> response = new ResponseOKHandler<string>(Message.DataDeleted);
+
+            return Ok(response);
         }
-
-        var result = _repository.Delete(entity);
-
-        if (!result)
+        catch (ExceptionHandler ex)
         {
-            return BadRequest("Failed to delete data");
-        }
+            ResponseErrorHandler response = new ResponseErrorHandler
+            {
+                Code = StatusCodes.Status500InternalServerError,
+                Status = HttpStatusCode.InternalServerError.ToString(),
+                Message = Message.FailedToCreateData,
+                Error = ex.Message
+            };
 
-        return Ok("Data deleted");
+            return StatusCode(StatusCodes.Status500InternalServerError, response);
+        }
     }
 }
